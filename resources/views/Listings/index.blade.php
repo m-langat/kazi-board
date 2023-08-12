f11b606987bb05300d648142688d8e92a3ec4116
Michael Langat <langatkipmichael@gmail.com>
2023-08-12 20:56:51 +0300

    added basic sytling to the listing index

--- resources/views/Listings/index.blade.php
+++ resources/views/Listings/index.blade.php
@@ -6,6 +6,13 @@
 <a href="/listings/new">Add a new job</a>
 
 @foreach ($listings as $listing)
-    <p>{{$listing->listing_title}}</p>
-@endforeach    
+    <div class="card">
+        <div class="card-title">
+            <a href="/listings/show/{{$listing->id}}"> {{$listing->listing_title}}</a>
+        </div>
+        <div class="card-info">
+            Date posted: {{$listing->listing_date}}
+        </div>
+    </div>
+@endforeach
 @endsection
