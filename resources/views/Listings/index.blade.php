92f02e7694fe62feea373c8b6037d95f2a4f65f2
Michael Langat <langatkipmichael@gmail.com>
2023-10-29 15:28:05 +0300

    added styling for the listing index page

--- resources/views/Listings/index.blade.php
+++ resources/views/Listings/index.blade.php
@@ -1,16 +1,39 @@
 @extends('layout')
 @section('content')
 
-<h1>Listings</h1>
 
-<a href="/listings/new">Add a new job</a>
+<div class="row justify-content-between align-items-center mt-4 mb-2">
+    
+        <div class="col-8">
+            <h1>Job Listings</h1>
+        </div>
+        <div class="col-4 d-flex justify-content-end">
+            <div>
+                <a class="btn btn-success btn-sm" href="/listings/new">Add listing</a>
+            </div>
+        </div>
+
+    
+</div>
+
+
 
 <div class="listings">
 
     @foreach ($listings as $listing)
     <div class="listing">
-        <a href="listings/{{$listing->id}}">{{$listing->listing_title}}</a>
+        <a href="listings/{{$listing->id}}">
+            <div class="card">
+                <div class="card-body">
+                    {{$listing->listing_title}}
+                </div>
+                <div class="card-footer text-body-secondary">
+                    Listed: {{$listing->listing_date}}   |   Expiring: {{$listing->listing_expiry_date}}
+                </div>
+            </div>
+        </a>
     </div>
+
         
     @endforeach    
 </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,13 +19,10 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                <a class="nav-link"  href="/listings">Job Listings</a>
-              </li>
+                    <a class="nav-link"  href="/listings">Job Listings</a>
+                </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            {{-- Right Navigation Menu --}}
             <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
