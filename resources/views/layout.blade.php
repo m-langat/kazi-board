5212fcfb44534fde1981c4894a2993501cad8b66
Michael Langat <langatkipmichael@gmail.com>
2023-10-29 13:12:43 +0300

    configure bootrap and webpack

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kazi-Board</title>
+
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
 </head>
 <body>
-    @yield('content')
+    <nav>
+
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
